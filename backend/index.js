9208879c885d7e4b02ab5b6f7e0e01930f14e2d0
Alexey <alexey.krupenia@gmail.com>
2023-12-27 00:32:51 +0300

    feat: implement back and front in one repo

---
+++ backend/index.js
@@ -0,0 +1,63 @@
+import express from 'express';
+import * as mqtt from 'mqtt';
+import cors from 'cors';
+
+const app = express()
+const mqttClient = mqtt.connect('mqtt://localhost:1883')
+
+// Connect to the MQTT broker
+mqttClient.on('connect', function () {
+    console.log('Connected to MQTT broker')
+})
+
+app.use(cors({
+    origin: '*'
+}));
+
+app.use(express.urlencoded());
+app.use(express.json());
+
+app.use((req, res, next) => {
+    res.set('Access-Control-Allow-Headers', 'X-Requested-With,Content-Type,Allow,Access-Control-Allow-Origin');
+    next();
+})
+
+// MQTT middleware for publishing and subscribing
+app.use(function (req, res, next) {
+    // Publish messages
+    req.mqttPublish = function (topic, message) {
+        mqttClient.publish(topic, message)
+    }
+
+    // Subscribe to topic
+    req.mqttSubscribe = function (topic, callback) {
+        mqttClient.subscribe(topic)
+        mqttClient.on('message', function (t, m) {
+            if (t === topic) {
+                callback(m.toString())
+            }
+        })
+    }
+    next()
+})
+
+app.get('/', function (req, res) {
+    // Subscribe
+    req.mqttSubscribe('sdvarh', function (message) {
+        console.log('Received message: ' + message)
+    })
+
+    res.send('MQTT is working!')
+})
+
+app.post('/topic', function (req, res) {
+    const { topic, payload } = req.body;
+
+    req.mqttPublish('sdvarh', JSON.stringify([topic, payload]));
+
+    res.send('MQTT is working!')
+})
+
+app.listen(3000, function () {
+    console.log('Server is running on port 3000')
+})
